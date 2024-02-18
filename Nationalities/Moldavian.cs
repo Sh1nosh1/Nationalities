ed9bcd74c6ed816fb73a44821823eb3952c61642
Shinda <123310732+Sh1nosh1@users.noreply.github.com>
2024-02-18 20:10:29 +0200

    Update Moldavian.cs

--- Nationalities/Moldavian.cs
+++ Nationalities/Moldavian.cs
@@ -8,13 +8,26 @@ using static System.Console;
 
 namespace Nationalities
 {
+    /// <summary>
+    /// Production class from Human
+    /// </summary>
     class Moldavian : Human
     {
+        /// <summary>
+        /// Constructor without parameters
+        /// </summary>
         public Moldavian() { }
+        /// <summary>
+        /// Constructor with 1 parameter
+        /// </summary>
+        /// <param name="name">Moldavian name</param>
         public Moldavian(string name) { Name = name; }
+        /// <summary>
+        /// Overriden method
+        /// </summary>
         public override void Greetings()
         {
             WriteLine($"Бунэ зиуа, {Name}!");
         }
-    }
+      }
 }

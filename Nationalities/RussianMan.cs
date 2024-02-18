d9e54e4f2cf389225d13199adb234dc6f76c0f12
Shinda <123310732+Sh1nosh1@users.noreply.github.com>
2024-02-18 20:10:59 +0200

    Update RussianMan.cs

--- Nationalities/RussianMan.cs
+++ Nationalities/RussianMan.cs
@@ -7,13 +7,26 @@ using static System.Console;
 
 namespace Nationalities
 {
+    /// <summary>
+    /// Production class from Human
+    /// </summary>
     class RussianMan : Human
     {
+        /// <summary>
+        /// Constructor without parameters
+        /// </summary>
         public RussianMan() { }
+        /// <summary>
+        /// Constructor with 1 parameter
+        /// </summary>
+        /// <param name="name"></param>
         public RussianMan(string name) { Name = name; }
+        /// <summary>
+        /// Overriden method
+        /// </summary>
         public override void Greetings()
         {
             WriteLine($"Привет, {Name}!");
         }
-    }
+      }
 }
